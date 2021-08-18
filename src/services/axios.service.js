1f8ff887ba9d76d57ccd5dd22ef15030a63cd7b9
caiocosta08 <caiocostaee@gmail.com>
2021-08-18 12:22:45 -0300

    correct server url

--- src/services/axios.service.js
+++ src/services/axios.service.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://192.168.0.9:3333'; //servidor
+// axios.defaults.baseURL = 'http://192.168.0.9:3333'; //servidor
+axios.defaults.baseURL = 'http://18.218.38.239/esports_api/'; //servidor
 
 export default axios;
